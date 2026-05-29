324c5d7ad8b8d3d45b3f79ffe152799c4b9ac2dc
Hung Nguyen <hungnb94@gmail.com>
2026-05-29 20:02:04 +0700

    feat: auth module public API

---
+++ src/auth/index.ts
@@ -0,0 +1,10 @@
+export {
+  getCredentials,
+  saveCredentials,
+  clearCredentials,
+  getConfigPath,
+} from './config.js'
+export type { Credentials } from './config.js'
+
+export { validateCredentials } from './credentials.js'
+export type { UserInfo } from './credentials.js'
