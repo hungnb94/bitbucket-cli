3d04e7b108e11d7ecb0371f94f3669ace8ff24df
Hung Nguyen <hungnb94@gmail.com>
2026-05-31 09:30:12 +0700

    fix: read version from package.json dynamically

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { Command } from 'commander'
 import { createAuthCommand } from './commands/auth.js'
 import { createPrCommand } from './commands/pr.js'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const packageJson = require('../package.json')
 
 const program = new Command()
 
 program
   .name('bitbucket')
   .description('Bitbucket CLI tool')
-  .version('0.1.0')
+  .version(packageJson.version)
 
 program.addCommand(createAuthCommand())
 program.addCommand(createPrCommand())
